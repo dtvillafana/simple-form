5d22edc60118b5679d715d9214a4debe67c29434
David Villafana <dvillafanaiv@protonmail.com>
2022-08-02 19:55:41 +0000

    added banner

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, send_from_directory, flash
+from flask import Flask, render_template, send_from_directory, flash, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Email
